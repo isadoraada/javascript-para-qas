d1da4a6cfe8da44130391b382d2a4199405b6652
isadora.de.araujo <isadora.de.araujo@avanade.com>
2025-10-06 17:34:30 -0300

    Página estilizada

--- js/app.js
+++ js/app.js
@@ -1,83 +1,52 @@
 const LIST = [
-    {
-        id: 1,
-        name: 'Mestre Yoda',
-        avatar: 'images/yoda.png'
-    },
-    {
-        id: 2,
-        name: 'Luke Skywalker',
-        avatar: 'images/luke.png'
-    },
-    {
-        id: 3,
-        name: 'Princesa Leia',
-        avatar: 'images/leia.png'           
-    },
-    {
-        id: 4,
-        name: 'Han Solo',
-        avatar: 'images/hansolo.png'
-    },
-    {
-        id: 5,
-        name: 'Darth Vader',
-        avatar: 'images/vader.png'
-    },
-    {
-        id: 6,
-        name: 'Chewbacca',
-        avatar: 'images/chewbacca.png'
-    },
-    {
-        id: 7,
-        name: 'R2D2',
-        avatar: 'images/r2d2.png'
-    },
-    {
-        id: 8,
-        name: 'C3PO',
-        avatar: 'images/c3po.png'
-    }
-]
+    { id: 1, name: 'Mestre Yoda', avatar: 'images/yoda.png' },
+    { id: 2, name: 'Luke Skywalker', avatar: 'images/luke.png' },
+    { id: 3, name: 'Princesa Leia', avatar: 'images/leia.png' },
+    { id: 4, name: 'Han Solo', avatar: 'images/hansolo.png' },
+    { id: 5, name: 'Darth Vader', avatar: 'images/vader.png' },
+    { id: 6, name: 'Chewbacca', avatar: 'images/chewbacca.png' },
+    { id: 7, name: 'R2D2', avatar: 'images/r2d2.png' },
+    { id: 8, name: 'C3PO', avatar: 'images/c3po.png' }
+];
 
 const App = new Vue({
     el: '#app',
     data: {
         title: 'Star Wars Lego',
-        userName: 'Isadora', 
+        userName: 'Isadora',
         characters: LIST,
-        searchName: ''
+        searchName: '',
+        customAlertMessage: '',
+        showCustomAlert: false
     },
     methods: {
         like(userName) {
-            alert(`O personagem ${userName} foi curtido!`)
+            this.showAlert(`O personagem ${userName} foi curtido!`);
         },
         remove(id) {
-            const allCharacters = this.characters
-            
-            const result = allCharacters.filter(item => {
-                return item.id !== id
-            })
-
-            this.characters = result
+            this.characters = this.characters.filter(item => item.id !== id);
         },
         search() {
-            if (this.searchName === '') {
-               return alert('Por favor, insira um nome para pesquisar!')
-            } 
+            if (this.searchName.trim() === '') {
+            return this.showAlert('Por favor, insira um nome para pesquisar!')
+        }
 
-            const allCharacters = this.characters = LIST
-            
-            const result = allCharacters.filter(item => {
-                return item.name === this.searchName
-            })
+        const result = LIST.filter(item =>
+        item.name.toLowerCase().includes(this.searchName.toLowerCase())
+        )
 
-            if (result.length <= 0) {
-                alert('Nenhum personagem encontrado!')
-            }
+        if (result.length === 0) {
+        return this.showAlert('Nenhum personagem encontrado!')
+    }
 
-            this.characters = result
+        this.characters = result
+        },
+        showAlert(message) {
+            this.customAlertMessage = message;
+            this.showCustomAlert = true;
+        },
+        closeAlert() {
+            this.showCustomAlert = false;
         }
     }
-})
+});
